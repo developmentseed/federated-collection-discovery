09acaf1235795b6686407c2d0b7da580b6b8e4c4
Henry Rodman <henry.rodman@gmail.com>
2025-10-15 06:40:33 -0500

    feat: ApiConfigPanel usability improvements (#130)
    * fix: fix docker setup
    
    * feat: clean up ApiConfigPanel
    
    * update changelog

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -10,7 +10,7 @@ const alertVariants = cva(
       variant: {
         default: "bg-background text-foreground",
         destructive:
-          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+          "border-red-500 dark:border-red-400 text-destructive dark:text-red-400 [&>svg]:text-destructive dark:[&>svg]:text-red-400",
       },
     },
     defaultVariants: {
